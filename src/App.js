c2fc6b096c9185a1dc53f098f0bdcc08db7b09e0
Justina <justan1997@gmail.com>
2021-10-06 20:20:58 +0300

    display posts html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { posts } from "./posts.js";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <h1>blog</h1>
+      {posts.map(({ id, title, body }) => (
+        <div key={id}>
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
