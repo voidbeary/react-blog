ed5e7895648662eb7efbaff7ad841f89861135d3
Justina Andrulionytė <69106997+voidbeary@users.noreply.github.com>
2021-10-22 19:12:15 +0300

    add localStorage (#9)
    * add localStorage
    
    * delete settings.json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import { posts as postsMocked } from "./posts.js";
+
+if (process.env.REACT_APP_USE_MOCK_POSTS === "true") {
+  localStorage.setItem("posts", JSON.stringify(postsMocked));
+}
 
 ReactDOM.render(
   <React.StrictMode>
