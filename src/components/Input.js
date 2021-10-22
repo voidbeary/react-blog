0c13950e13435c9c3c84bf5db93ea598f230d892
Justina Andrulionyte <69106997+voidbeary@users.noreply.github.com>
2021-10-22 20:24:53 +0300

    fix validation on submit

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,19 @@
 import ErrorMessage from "./ErrorMessage";
 import { useState } from "react";
 
-function InputForm({ value, onChange, label, id, required }) {
+function InputForm({ value, onChange, label, id, required, error }) {
   const [isBlurred, setIsBlurred] = useState(false);
 
-  function handleBlur() {
+  function handleBlur(e) {
+    onChange(e.target.value);
     setIsBlurred(true);
+  }
+  function handleChange(e) {
+    onChange(e.target.value);
   }
   const newLabel = required ? label + "*" : label;
 
-  const should_show_error = required && isBlurred && !value;
+  const should_show_error = error && isBlurred;
   const className = should_show_error
     ? "shadow-md focus:outline-none focus:ring-2 ring-1 ring-red-600 p-2 my-2 rounded-md"
     : "shadow-md focus:ring-2 focus:ring-blue-600 p-2 my-2  rounded-md";
@@ -21,7 +25,7 @@ function InputForm({ value, onChange, label, id, required }) {
         id={id}
         placeholder={id}
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
         className={className}
         onBlur={handleBlur}
       ></input>
