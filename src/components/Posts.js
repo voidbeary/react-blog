00d468f9047b06b5efe1c08316b2b05488d81c7b
Justina Andrulionytė <69106997+voidbeary@users.noreply.github.com>
2021-10-23 16:41:59 +0300

    add sorting by date (#18)

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,7 +2,7 @@ import Post from "./Post.js";
 import { getPosts } from "../utils/postsStorage.js";
 
 function Posts() {
-  const posts = getPosts();
+  const posts = getPosts().sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <>
